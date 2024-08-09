a1cd0e5172754f32b732ed9f4ed03c0070662af0
W1ndys <w1ndys@outlook.com>
2024-08-09 12:20:22 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+# script/example/main.py
+# 示例脚本
+# 本脚本写好了基本的函数，直接在函数中编写逻辑即可，必要的时候可以修改函数名
+
+import logging
+import os
+import sys
+
+sys.path.append(
+    os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+)
+
+
+async def handle_group_message(websocket, msg):
+    try:
+        user_id = msg.get("user_id")
+        group_id = msg.get("group_id")
+        raw_message = msg.get("raw_message")
+        role = msg.get("sender", {}).get("role")
+        message_id = msg.get("message_id")
+
+    except Exception as e:
+        logging.error(f"处理编解码消息失败: {e}")
+        return
+
+
+async def handle_private_message(websocket, msg):
+    try:
+        user_id = msg.get("user_id")
+        raw_message = msg.get("raw_message")
+
+    except Exception as e:
+        logging.error(f"处理编解码消息失败: {e}")
+        return
